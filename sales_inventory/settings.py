52929712106acd47009afd42513794d2715dc227
Shubhangi Chaturvedi <79640121+shubhangichaturvedi02@users.noreply.github.com>
2021-10-30 22:58:41 +0530

    Update settings.py

--- sales_inventory/settings.py
+++ sales_inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lw7t*&(fd&@0f#4qi8-@ihqw#n^p=h_yu*93g^%rz7z9+y04q-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
